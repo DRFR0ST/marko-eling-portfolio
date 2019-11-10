9cc001426d41c191ac9c92925b46981045617263
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-10 22:37:07 +0000

    Deploy DRFR0ST/marko-eling-portfolio to github.com/DRFR0ST/marko-eling-portfolio.git:gh-pages

---
+++ precache-manifest.81c7ecd4a8e55aab065c932e6bf2a0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d277f27f3d067b3a28f4d3fa1b03c88",
+    "url": "/marko-eling-portfolio/index.html"
+  },
+  {
+    "revision": "fede942f40fb9e591ed9",
+    "url": "/marko-eling-portfolio/static/css/main.1de8421a.chunk.css"
+  },
+  {
+    "revision": "84bd3ed2f8d7c7a9a5a6",
+    "url": "/marko-eling-portfolio/static/js/2.0f6c901e.chunk.js"
+  },
+  {
+    "revision": "fede942f40fb9e591ed9",
+    "url": "/marko-eling-portfolio/static/js/main.d2aaa4c3.chunk.js"
+  },
+  {
+    "revision": "ccf26ae3f9d7a9540d63",
+    "url": "/marko-eling-portfolio/static/js/runtime-main.d9b21d86.js"
+  },
+  {
+    "revision": "5db7de97aa9d1af30ce6e4a678554ec1",
+    "url": "/marko-eling-portfolio/static/media/header_illustration.5db7de97.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9dd7f45503dd37a6c4e96ffab40a0ea2.js"
+  "/marko-eling-portfolio/precache-manifest.81c7ecd4a8e55aab065c932e6bf2a0e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/marko-eling-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
