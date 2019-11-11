725607d16e0f62defc2e183d60012309e9eb876a
Mike Eling <mike.eling97@gmail.com>
2019-11-11 17:59:09 +0100

    :bento: Make header responsive

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import illustration from "assets/header_illustration.svg";
+import { IconButton, Icon } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,12 +17,29 @@ const useStyles = makeStyles(theme => ({
     },
     "@media (max-width: 767px)": {
         flexDirection: "column",
+        justifyContent: "center",
         "& img": {
-            maxWidth: "90%",
+          display: "none",
+          maxWidth: "65%",
         }
     }
   },
+  downIcon: {
+    justifySelf: "flex-end",
+    height: "15%",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+    "& p":{
+      marginBottom: theme.spacing()
+    },
+    "@media (min-width: 767px)": {
+      display: "none",
+    }
+  },
   slogan: {
+    margin: "auto",
       "& h1": {
           color: theme.palette.text.main,
           fontSize: 68,
@@ -44,6 +62,10 @@ const Header = () => {
           <h1>Clouds.</h1>
           <h1>Pipelines.</h1>
           <h1>Networks.</h1>
+        </div>
+        <div className={classes.downIcon}>
+            <p style={{textAlign: "center"}}>Learn more</p>
+            <IconButton><Icon>keyboard_arrow_down</Icon></IconButton>
         </div>
         <img src={illustration} alt="header" />
     </div>
