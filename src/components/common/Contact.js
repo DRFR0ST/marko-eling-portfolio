c81b6211807426dd5e20922240162fed703ae001
Mike Eling <mike.eling97@gmail.com>
2019-11-11 20:44:27 +0100

    :art: Make contact form send emails

--- src/components/common/Contact.js
+++ src/components/common/Contact.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { TextField, Button } from "@material-ui/core";
-import Icon from '@material-ui/core/Icon';
+import { TextField, Button, IconButton, Tooltip, useTheme } from "@material-ui/core";
+import Icon from "@material-ui/core/Icon";
+import emailjs from "emailjs-com";
+import { UndrawAstronaut } from "react-undraw";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,7 +11,7 @@ const useStyles = makeStyles(theme => ({
     position: "relative",
     width: "100%",
     display: "flex",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   content: {
     position: "relative",
@@ -19,25 +21,24 @@ const useStyles = makeStyles(theme => ({
     minHeight: "300px",
     width: "75%",
     border: `1px solid ${theme.palette.border}`,
-    background: theme.palette.background.dark,
+    background: theme.palette.background.main,
     padding: "1.5vh 1.5vw 0.5vh",
     borderRadius: 14,
     "@media (max-width: 982px)": {
       flexDirection: "column-reverse",
       width: "90%",
-      padding: "1vh 0",
+      padding: "1vh 0"
     }
   },
   infoContainer: {
     width: "30%",
-    background: theme.palette.background.dark,
-    padding: theme.spacing()*3,
+    padding: theme.spacing() * 3,
     "@media (max-width: 982px)": {
       position: "relative",
       top: "-5%"
     },
     "& span": {
-      "@media (prefers-color-scheme: dark)": { 
+      "@media (prefers-color-scheme: dark)": {
         color: theme.palette.text.main
       }
     },
@@ -47,12 +48,12 @@ const useStyles = makeStyles(theme => ({
   },
   formContainer: {
     position: "relative",
-    padding: theme.spacing()*3,
+    padding: theme.spacing() * 3,
     width: "65%",
-    border: `1px solid ${theme.palette.border}`,
+    //border: `1px solid ${theme.palette.border}`,
     background: theme.palette.background.main,
     borderRadius: 14,
-    //boxShadow: "-15px 15px 25px rgba(0, 0, 0, 0.05)",
+    boxShadow: "-15px 15px 20px rgba(0, 0, 0, 0.085)",
     right: "-10%",
     top: "-15%",
     "@media (max-width: 982px)": {
@@ -60,23 +61,25 @@ const useStyles = makeStyles(theme => ({
       left: "50%",
       width: "90%",
       transform: "translateX(-50%)",
-      top: "-10%"
+      top: "-10%",
+      boxShadow: "0px 15px 20px rgba(0, 0, 0, 0.085)"
     }
   },
-  doubleField:{
+  doubleField: {
     display: "flex",
     justifyContent: "flex-start",
     alignItems: "flex-start",
-    width: "100%",
+    width: "100%"
   },
   label: {
-    color: theme.palette.text.main,
+    color: theme.palette.text.main
   },
   infoItem: {
     display: "flex",
     alignItems: "center",
     "& p": {
-      marginLeft: theme.spacing()*2
+      marginLeft: theme.spacing() * 2,
+      borderBottom: `5px solid ${theme.palette.primary.main}`
     }
   },
   "@media (prefers-color-scheme: dark)": {
@@ -85,7 +88,7 @@ const useStyles = makeStyles(theme => ({
       "&:hover, &:focus": {
         background: "rgba(255, 255, 255, 0.12)",
         ".MuiFilledInput-underline:hover:before": {
-            background: "rgba(255, 255, 255, 0.14)",
+          background: "rgba(255, 255, 255, 0.14)"
         }
       },
       buttonRoot: {
@@ -95,61 +98,176 @@ const useStyles = makeStyles(theme => ({
     },
     underline: {
       "&:before": {
-        borderColor: "rgba(255, 255, 255, 0.12)",
+        borderColor: "rgba(255, 255, 255, 0.12)"
       }
     }
+  },
+  errorInfo: {
+    color: "red"
   }
 }));
 
 const Contact = () => {
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [messageValue, setMessageValue] = useState("");
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [messageError, setMessageError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(null);
+
+  const theme = useTheme();
   const classes = useStyles();
 
   const handleSubmit = e => {
+    const templateId = "template_IYsB7SbI";
+
+    if(nameValue && emailValue && messageValue)
+      sendFeedback(templateId, {
+        message_html: messageValue,
+        from_name: nameValue,
+        reply_to: emailValue
+      });
+    else {
+      if(!nameValue) setNameError("Can't be empty");
+      if(!emailValue) setEmailError("Can't be empty");
+      if(!messageValue) setMessageError("Can't be empty");
+    }
     e.preventDefault();
-  }
+  };
+
+  const sendFeedback = (templateId, variables) => {
+    try {
+      emailjs
+        .send("gmail", templateId, variables, process.env.REACT_APP_EMAILJS_ID)
+        .then(res => {
+          setSuccess(true);
+        })
+        // Handle errors here however you like, or use a React error boundary
+        .catch(err => setError(err.message));
+    } catch(err) {
+      setError(err.message || err)
+    }
+  };
 
   const textFieldProps = {
-      InputProps: {
-        classes: {root: classes.inputRoot, underline: classes.underline}
-      },
-      InputLabelProps: {
-        classes: {root: classes.label}
-      }
-  }
+    InputProps: {
+      classes: { root: classes.inputRoot }
+    },
+    InputLabelProps: {
+      classes: { root: classes.label }
+    }
+  };
+
+  const handleChange = (v, fn) => {
+    fn && fn(v);
+    setError(null);
+  };
 
   const contactInfo = [
-    {icon: "mail", value: "marko.eling@gmail.com"},
-    {icon: "phone", value: "+49 XXX-XXX-XXX"},
-    {icon: "home", value: "Berlin, Germany"},
-  ]
+    { icon: "mail", value: "marko.eling@gmail.com" },
+    { icon: "phone", value: "+49 XXX-XXX-XXX" },
+    { icon: "home", value: "Berlin, Germany" }
+  ];
 
   return (
     <div className={classes.root}>
-        <div className={classes.content}>
-          <div className={classes.infoContainer}>
-            <h1 style={{opacity: 0.8}}>Contact</h1>
-            {
-              contactInfo.map(e => <div className={classes.infoItem}><Icon>{e.icon}</Icon> <p className="mono" >{e.value}</p></div>)
-            }
-          </div>
-          <div className={classes.formContainer}>
-            <h2 style={{opacity: 0.75}}>Contact me</h2>
-            <form onSubmit={handleSubmit}>
-              <div className={classes.doubleField}>
-                <TextField {...textFieldProps} label="Name" variant="filled" />
-                <TextField {...textFieldProps} label="Email" variant="filled" style={{marginLeft: "6px"}} type="email" />
-              </div>
-              <br />
-              <TextField {...textFieldProps} multiline label="Message" style={{width: "100%", marginBottom: "10px"}} variant="filled" />
-              <br />
-              <br />
-              <Button classes={{root: classes.buttonRoot}} variant="outlined" size="large" align="right" style={{float: "right"}}>Submit</Button>
-              <div style={{clear: "both"}} />
-            </form>
-          </div>
+      <div className={classes.content}>
+        <div className={classes.infoContainer}>
+          <h1 style={{ opacity: 0.8 }}>Contact</h1>
+          {contactInfo.map(e => (
+            <div key={e.value} className={classes.infoItem}>
+              <Icon>{e.icon}</Icon> <p className="mono">{e.value}</p>
+            </div>
+          ))}
+          <Tooltip title="Download CV" aria-label="download cv">
+            <IconButton
+              style={{ float: "right" }}
+              href="http://www.africau.edu/images/default/sample.pdf"
+              target="_blank"
+            >
+              <Icon>file_copy</Icon>
+            </IconButton>
+          </Tooltip>
         </div>
+        <div className={classes.formContainer}>
+          {!success && (
+            <React.Fragment>
+              <h2 style={{ opacity: 0.75 }}>Contact me</h2>
+              <form onSubmit={() => null}>
+                <div className={classes.doubleField}>
+                  <TextField
+                    error={nameError}
+                    {...textFieldProps}
+                    value={nameValue}
+                    onChange={e => handleChange(e.target.value, setNameValue)}
+                    label="Name"
+                    variant="outlined"
+                  />
+                  <TextField
+                    error={emailError || validateEmail(emailValue) ? false : "Invalid format"}
+                    {...textFieldProps}
+                    value={emailValue}
+                    onChange={e => handleChange(e.target.value, setEmailValue)}
+                    label="Email"
+                    variant="outlined"
+                    style={{ marginLeft: "6px" }}
+                    type="email"
+                  />
+                </div>
+                <br />
+                <TextField
+                  {...textFieldProps}
+                  value={messageValue}
+                  error={messageError}
+                  onChange={e => handleChange(e.target.value, setMessageValue)}
+                  multiline
+                  label="Message"
+                  style={{ width: "100%", marginBottom: "10px" }}
+                  variant="outlined"
+                />
+                <br />
+                {error && (
+                  <p className={classes.errorInfo}>
+                    <Icon style={{ marginRigth: "10px" }}>warning</Icon> {error}
+                  </p>
+                )}
+                <br />
+                <Button
+                  type="button"
+                  variant="contained"
+                  classes={{ root: classes.buttonRoot }}
+                  size="large"
+                  align="right"
+                  color="primary"
+                  onClick={handleSubmit}
+                  style={{ float: "right", color: "#fff" }}
+                >
+                  Submit{" "}
+                  <Icon style={{ marginLeft: "10px", fontSize: "18px" }}>
+                    send
+                  </Icon>
+                </Button>
+                <div style={{ clear: "both" }} />
+              </form>
+            </React.Fragment>
+          )}
+          {success && (
+            <React.Fragment>
+              <UndrawAstronaut height="300px" primaryColor={theme.palette.primary.main} />
+            </React.Fragment>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Contact;
+
+function validateEmail(email) {
+  if(email.length === 0) return true;
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import "./index.css";
 import * as serviceWorker from './serviceWorker';
+import emailjs from "emailjs-com";
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+emailjs.init(process.env.REACT_APP_EMAILJS_ID);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
